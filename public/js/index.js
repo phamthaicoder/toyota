dbdf5a2af9d93f46e18b91058ce8512eff9c1c4d
nambui <nambp@amaisoft.com>
2022-07-09 12:57:50 +0700

    config scss

---
+++ public/js/index.js
@@ -0,0 +1,80 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************!*\
+  !*** ./resources/js/index.js ***!
+  \*******************************/
+var readMoreAll = document.querySelectorAll('.read-more');
+var collapseAll = document.querySelectorAll('.collapse');
+readMoreAll.forEach(function (readMore) {
+  return readMore.addEventListener("click", function (e) {
+    e.target.nextSibling.nextSibling.style.display = 'contents';
+    readMore.style.display = 'none';
+    e.target.nextSibling.nextSibling.nextSibling.nextSibling.style.display = 'contents';
+  });
+});
+collapseAll.forEach(function (collapse) {
+  return collapse.addEventListener("click", function (e) {
+    e.target.previousSibling.previousSibling.style.display = 'none';
+    collapse.style.display = 'none';
+    e.target.previousSibling.previousSibling.previousSibling.previousSibling.style.display = 'contents';
+  });
+});
+var sliderImageAll = document.querySelectorAll('.slider-image');
+sliderImageAll.forEach(function (sliderImage) {
+  return sliderImage.addEventListener("mouseover", function (e) {
+    sliderImage.classList.add('active');
+  });
+});
+sliderImageAll.forEach(function (sliderImage) {
+  return sliderImage.addEventListener("mouseout", function (e) {
+    sliderImage.classList.remove('active');
+  });
+}); // let scrollpos = window.innerHeight;
+// const monday = document.getElementById("monday");
+// const mondayAnimate =  monday.childNodes[1];
+// const monday_height = monday.offsetHeight;
+// const add_class_on_scroll = () => {
+//     mondayAnimate.classList.add("animate");
+// }
+// const remove_class_on_scroll = () => {
+//     mondayAnimate.classList.remove("animate");
+// }
+// window.addEventListener('scroll', function() { 
+//     console.log(scrollpos, monday_height)
+//   if (scrollpos = monday_height) { add_class_on_scroll() }
+//   else { remove_class_on_scroll() };
+// })
+
+function reveal() {
+  var reveals = document.querySelectorAll(".background-section");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("kenburns-top");
+    } else {
+      reveals[i].classList.remove("kenburns-top");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal); // To check the scroll position on page load
+
+reveal();
+var htmlElement = document.querySelector("html");
+var modalAll = document.querySelectorAll('.modal');
+modalAll.forEach(function (modal) {
+  return modal.addEventListener('show.bs.modal', function (event) {
+    htmlElement.style.overflowY = "hidden";
+  });
+});
+modalAll.forEach(function (modal) {
+  return modal.addEventListener('hide.bs.modal', function (event) {
+    htmlElement.style.overflowY = "unset";
+  });
+});
+/******/ })()
+;
